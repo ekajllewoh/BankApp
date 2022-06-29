9028f9b5aa98679f28a517b61dd7e609ff2df105
Jake Howell <jhowel44@uncc.edu>
2022-06-29 09:26:18 -0400

    Final

--- src/main/java/com/revature/App.java
+++ src/main/java/com/revature/App.java
@@ -45,205 +45,202 @@ public class App {
 			System.out.println("Welcome to your account: " + loggedInUser.getUsername());
 
 			AccountService as = new AccountService();
-			
+
 			System.out.println(loggedInUser.getRole() == Role.Customer);
-			
-			if(loggedInUser.getRole() == Role.Customer) {
 
-			System.out.println("If you would like to open an account press 1, if you would like to view your accounts press 2. Press 0 to close.");
+			if (loggedInUser.getRole() == Role.Customer) {
 
-			int existingAccountInput = -1;
+				System.out.println(
+						"If you would like to open an account press 1, if you would like to view your accounts press 2. Press 0 to close.");
 
-			while (existingAccountInput != 0) {
+				int existingAccountInput = -1;
 
-				existingAccountInput = scan.nextInt();
+				while (existingAccountInput != 0) {
 
-				if (existingAccountInput == 1) {
-					int newAccId = as.createAccount(loggedInUser.getId());
-					System.out.println("Created account with id " + newAccId);
-					System.out.println("Please wait for administrator approval to activate your account.");
-				} else if (existingAccountInput == 2) {
-					System.out.println("Here are your accounts: ");
-					as.viewAllAccounts(loggedInUser.getId());
-					System.out.println("Select an account by its ID for a withdrawal, deposit or transfer.");
-					int accountOption = scan.nextInt(); 
-					
-					if(as.checkAccount(loggedInUser.getId(), accountOption)) {
-						
-						if(as.checkActiveAccount(accountOption)) {
-							
-							System.out.println("Select 1 for withdrawl, 2 for deposit, 3 for transfer.");
-							int moneyMover = scan.nextInt();
-							
-							if(moneyMover == 1) {
-								System.out.println("How much do you wish to withdraw?");
-								double moneyAmount = scan.nextDouble();
-								
-								if(moneyAmount > as.checkBalance(accountOption)) {
-									as.addMoneyToAccount(accountOption, moneyMover);
-								}else {
-									System.out.println("Please do not overdraft.");
+					existingAccountInput = scan.nextInt();
+
+					if (existingAccountInput == 1) {
+						int newAccId = as.createAccount(loggedInUser.getId());
+						System.out.println("Created account with id " + newAccId);
+						System.out.println("Please wait for administrator approval to activate your account.");
+					} else if (existingAccountInput == 2) {
+						System.out.println("Here are your accounts: ");
+						as.viewAllAccounts(loggedInUser.getId());
+						System.out.println("Select an account by its ID for a withdrawal, deposit or transfer.");
+						int accountOption = scan.nextInt();
+
+						if (as.checkAccount(loggedInUser.getId(), accountOption)) {
+
+							if (as.checkActiveAccount(accountOption)) {
+
+								System.out.println("Select 1 for withdrawl, 2 for deposit, 3 for transfer.");
+								int moneyMover = scan.nextInt();
+
+								if (moneyMover == 1) {
+									System.out.println("How much do you wish to withdraw?");
+									double moneyAmount = scan.nextDouble();
+
+									if (moneyAmount > as.checkBalance(accountOption)) {
+										as.addMoneyToAccount(accountOption, moneyMover);
+									} else {
+										System.out.println("Please do not overdraft.");
+									}
+
+								} else if (moneyMover == 2) {
+									System.out.println("How much do you wish to deposit?");
+									double moneyAmount = scan.nextDouble();
+									as.withdrawMoneyFromAccount(accountOption, moneyAmount);
+
+								} else if (moneyMover == 3) {
+									System.out.println("How much do you wish to transfer?");
+									double moneyAmount = scan.nextDouble();
+									System.out.println("What account number do you wish to transfer to?");
+									int recievingAcc = scan.nextInt();
+									if (moneyAmount > as.checkBalance(accountOption)) {
+										as.transferMoneyToAccount(accountOption, moneyAmount, recievingAcc);
+									} else {
+										System.out.println("Please do not tranfer more than the account has.");
+									}
+
+								} else {
+									System.out.println("Please select a valid option.");
 								}
-								
-								
-							}else if(moneyMover == 2) {
-								System.out.println("How much do you wish to deposit?");
-								double moneyAmount = scan.nextDouble();
-								as.withdrawMoneyFromAccount(accountOption, moneyAmount);
-								
-							}else if(moneyMover == 3) {
-								System.out.println("How much do you wish to transfer?");
-								double moneyAmount = scan.nextDouble();
-								System.out.println("What account number do you wish to transfer to?");
-								int recievingAcc = scan.nextInt();
-								if(moneyAmount > as.checkBalance(accountOption)) {
-									as.transferMoneyToAccount(accountOption, moneyAmount, recievingAcc);
-								}else {
-									System.out.println("Please do not tranfer more than the account has.");
-								}
-								
-								
-								
-							}else {
-								System.out.println("Please select a valid option.");
+
+							} else if (!as.checkActiveAccount(accountOption)) {
+								System.out.println("This account has not been activated yet.");
+							} else {
+								System.out.println("How did you even get here?");
 							}
-							
-							
-						}else if(!as.checkActiveAccount(accountOption)){
-							System.out.println("This account has not been activated yet.");
-						}else {
+
+						} else if (!as.checkAccount(loggedInUser.getId(), accountOption)) {
+
+							System.out.println("Please select an account registered to this user.");
+
+						} else {
 							System.out.println("How did you even get here?");
 						}
-						
-					}else if(!as.checkAccount(loggedInUser.getId(), accountOption)) {
-						
-						System.out.println("Please select an account registered to this user.");
-						
-					}else {
-						System.out.println("How did you even get here?");
+
+					} else {
+						System.out.println("I'm sorry that wasnt a valid option. Please try again.");
 					}
-					
-					
-				} else {
-					System.out.println("I'm sorry that wasnt a valid option. Please try again.");
+					System.out.println(
+							"If you would like to open an account press 1, if you would like to view your accounts press 2. Press 0 to close.");
 				}
-				System.out.println("If you would like to open an account press 1, if you would like to view your accounts press 2. Press 0 to close.");
-			}
-			}else if(loggedInUser.getRole() == Role.Employee) {
-				
-				System.out.println("Press 1 to view all accounts, or press 2 to approve/deny a specific account. Press 0 to end.");
+			} else if (loggedInUser.getRole() == Role.Employee) {
+
+				System.out.println(
+						"Press 1 to view all accounts, or press 2 to approve/deny a specific account. Press 0 to end.");
 				int accViewer = scan.nextInt();
-				while(accViewer != 0) {
-				
-				if(accViewer == 1) {
-					as.viewAllAccounts();
-				}else if(accViewer == 2) {
-					System.out.println("Please enter the account number of the account you would like to view.");
-					
-					int specId = scan.nextInt();
-					as.viewAllAccounts(specId);
-					System.out.println("Please enter 1 to approve the account or 2 to deny the account.");
-					
-					int activeChecker = scan.nextInt();
-					
-					if(activeChecker == 1) {
-						as.setAccActive(specId, true);
-					}else if (activeChecker == 2) {
-						as.setAccActive(specId, false);
-					}else {
-						System.out.println("Not a valid action for the account");
+				while (accViewer != 0) {
+
+					if (accViewer == 1) {
+						as.viewAllAccounts();
+					} else if (accViewer == 2) {
+						System.out.println("Please enter the account number of the account you would like to view.");
+
+						int specId = scan.nextInt();
+						as.viewAllAccounts(specId);
+						System.out.println("Please enter 1 to approve the account or 2 to deny the account.");
+
+						int activeChecker = scan.nextInt();
+
+						if (activeChecker == 1) {
+							as.setAccActive(specId, true);
+						} else if (activeChecker == 2) {
+							as.setAccActive(specId, false);
+						} else {
+							System.out.println("Not a valid action for the account");
+						}
+
+					} else {
+						System.out.println("Please enter one of the listed options.");
 					}
-					
-				}else {
-					System.out.println("Please enter one of the listed options.");
 				}
-				}
-				
-			}else if(loggedInUser.getRole() == Role.Admin) {
+
+			} else if (loggedInUser.getRole() == Role.Admin) {
 				int nextCheck = -1;
-				while(nextCheck != 0)
-				System.out.println("Please enter 1 to view all accounts, 2 to access a specific account. Press 0 to exit.");
+				while (nextCheck != 0)
+					System.out.println(
+							"Please enter 1 to view all accounts, 2 to access a specific account. Press 0 to exit.");
 				nextCheck = scan.nextInt();
-				if(nextCheck == 1) {
-					
+				if (nextCheck == 1) {
+
 					as.viewAllAccounts();
-					
-				}else if(nextCheck == 2) {
-					
+
+				} else if (nextCheck == 2) {
+
 					System.out.println("Select the id of the account you wish to change.");
 					int adminId = scan.nextInt();
 					System.out.println("Press 1 to approve/deny, 2 to withdraw/deposit/transfer, 3 to cancel account.");
 					int adminChoice = scan.nextInt();
-					if(adminChoice == 1) {
-						
+					if (adminChoice == 1) {
+
 						System.out.println("Please enter 1 to approve the account or 2 to deny the account.");
-						
+
 						int activeChecker = scan.nextInt();
-						
-						if(activeChecker == 1) {
+
+						if (activeChecker == 1) {
 							as.setAccActive(adminId, true);
-						}else if (activeChecker == 2) {
+						} else if (activeChecker == 2) {
 							as.setAccActive(adminId, false);
-						}else {
+						} else {
 							System.out.println("Not a valid action for the account");
 						}
-						
-					}else if(adminChoice == 2) {
-						
+
+					} else if (adminChoice == 2) {
+
 						System.out.println("Please press 1 to withdraw, 2 to deposit or 3 to transfer.");
 						int nextChoice = scan.nextInt();
-						if(nextChoice == 1) {
+						if (nextChoice == 1) {
 							System.out.println("How much would you like to withdraw?");
 							double adminWithdraw = scan.nextDouble();
-							
+
 							as.withdrawMoneyFromAccount(adminId, adminWithdraw);
-							
-						}else if(nextChoice == 2) {
-							
+
+						} else if (nextChoice == 2) {
+
 							System.out.println("How much would you like to deposit?");
 							double adminDeposit = scan.nextDouble();
-							
+
 							as.addMoneyToAccount(adminId, adminDeposit);
-							
-						}else if(nextChoice == 3) {
-							
+
+						} else if (nextChoice == 3) {
+
 							System.out.println("How much would you like to transfer?");
 							double adminTransfer = scan.nextDouble();
 							System.out.println("Please list the account ID for the money to be transferred to.");
 							int adminNextAcc = scan.nextInt();
-							
+
 							as.transferMoneyToAccount(adminId, adminNextAcc, adminNextAcc);
-							
-						}else {
+
+						} else {
 							System.out.println("That was not a valid option for the account.");
 						}
-						
-						
-					}else if(adminChoice == 3) {
-						
+
+					} else if (adminChoice == 3) {
+
 						System.out.println("Confirm to cancel account with ID " + adminId + ". Press 1 to confirm.");
 						int adminConfirm = scan.nextInt();
-						
-						if(adminConfirm == 1) {
-							
+
+						if (adminConfirm == 1) {
+
 							as.deleteAcc(adminId);
-							
+
 							System.out.println("Account with " + adminId + " has been canceled.");
-							
-						}else {
+
+						} else {
 							System.out.println("Request not confirmed account with ID " + adminId + " not canceled.");
 						}
-						
-					}else {
+
+					} else {
 						System.out.println("Please choose a valid option.");
 					}
-					
-				}else {
+
+				} else {
 					System.out.println("Please enter a listed option.");
 				}
-				
-				
-			}else {
+
+			} else {
 				System.out.println("Unrecognized user role.");
 			}
 
